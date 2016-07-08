d7a2dd656068222dbf70bf2f2b833c09e996f640
Michelle Daya <michelle.daya@ucdenver.edu>
2016-07-08 11:04:52 -0600

    Comitting bash script to schedule QQ plot run

---
+++ run_qq.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#Set parameters
+if [ "$#" -eq  "0" ]
+the
+    echo "Usage: ${0##*/} <study_name> <cohort_name> <out_file_name> <err_p_file_name>"
+    echo "Eg:"
+    echo "bash run_qq.sh BRIDGE jhu_abr"
+    exit
+fi
+ study_name=$1
+ cohort_name=$2
+ in_file_name=/gpfs/barnes_share/caapa_metal/data/input/${cohort_name}.txt
+ out_file_name=../data/output/qq_plots/${cohort_name}.png
+ err_p_file_name=../data/output/err_p_vals/${cohort_name}.txt
+
+#Create output directories in case they do not exist
+ mkdir ../data/
+ mkdir ../data/output/
+ mkdir ../data/output/qq_plots
+ mkdir ../data/output/err_p_vals
+
+cat draw_qq.R | R --vanilla --args $study_name $in_file_name $out_file_name $err_p_file_name
