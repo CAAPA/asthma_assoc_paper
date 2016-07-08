524ebb77050e2f20f4761a36b889bab1c800eeed
Michelle Daya <michelle.daya@gmail>
2016-07-08 07:47:33 -0600

    Comitting initial QQ plot stab

---
+++ draw_qq.R
@@ -0,0 +1,32 @@
+study.name <- "BRIDGE"
+in.file.name <- "tmp_jhu_abr.txt"
+out.file.name <- "jhu_abr.png"
+err.p.file.name <- "jhu_small_p.txt"
+
+plotQQ <- function(p.vals, study.name, plot.type) {
+  observed <- sort(p.vals)
+  lobs <- -(log10(observed))
+  expected <- c(1:length(observed))
+  lexp <- -(log10(expected / (length(expected)+1)))
+  plot(c(0,10), c(0,10), col="red", lwd=1, type="l", 
+       xlab="Expected (-logP)", ylab="Observed (-logP)", 
+       xlim=c(0,10), ylim=c(0,10), 
+       las=1, xaxs="i", yaxs="i", bty="l", main=paste0(study.name, " (" , plot.type, ")"))
+  points(lexp, lobs, pch=23, cex=.1, bg="black")
+  chisq1 <- qchisq(p.vals,1,lower.tail = F)
+  inf.fact <- round(median(chisq1)/qchisq(0.5,1),4)
+  text(7, 2, bquote(lambda == .(inf.fact)))
+}
+
+#png(out.file.name, width=720, height=265)
+png(out.file.name, width=9.5, height=3.8, units = 'in', res = 200)
+par(mfrow=c(1,3), cex=1.2)
+results <- read.table("tmp_jhu_abr.txt", head=T, stringsAsFactors = F)
+write.table(results[results$PVALUE < 1e-9,], err.p.file.name,
+            sep="\t", quote=F, row.names=F, col.names=T)
+p.vals <- results$PVALUE
+freq <- results$MAF
+plotQQ(p.vals, study.name, "All")
+plotQQ(p.vals[freq >= 0.05], study.name, "Common")
+plotQQ(p.vals[freq < 0.05], study.name, "Rare")
+dev.off()
