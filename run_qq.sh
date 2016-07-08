8a78cb2378ef420b65648a51fe7bc20d737b923d
Michelle Daya <michelle.daya@ucdenver.edu>
2016-07-08 11:12:15 -0600

    Fixed to load R

--- run_qq.sh
+++ run_qq.sh
@@ -16,9 +16,13 @@ out_file_name=../data/output/qq_plots/${cohort_name}.png
 err_p_file_name=../data/output/err_p_vals/${cohort_name}.txt
 
 #Create output directories in case they do not exist
- mkdir ../data/
- mkdir ../data/output/
- mkdir ../data/output/qq_plots
- mkdir ../data/output/err_p_vals
+mkdir ../data/
+mkdir ../data/output/
+mkdir ../data/output/qq_plots
+mkdir ../data/output/err_p_vals
 
+#Load R
+module load R/3.2.5
+
+#Run QQ plot
 cat draw_qq.R | R --vanilla --args $study_name $in_file_name $out_file_name $err_p_file_name
